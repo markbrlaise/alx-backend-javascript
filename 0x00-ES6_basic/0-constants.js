e291ab02754555b0676598246bef6518c889bc1e
mark blaise <markkiguli10@gmail.com>
2023-06-27 11:50:15 +0300

    const or let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,9 @@
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+}
+
+export function taskNext() {
+    let taskNext = 'But sometimes let is okay';
+    return taskNext;
+}

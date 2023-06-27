a627f06530eb349d2ae20d08f8b630e7e56737cc
mark blaise <markkiguli10@gmail.com>
2023-06-27 20:36:00 +0300

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,11 @@
-import {uploadPhoto, createUser} from './utils';
+import {createUser, uploadPhoto} from './utils';
 
-export default function handleProfileSignup() {
-  const p1 = uploadPhoto();
-  const p2 = createUser();
-
-  return Promise.all([p1, p2]).then((value) => {
-    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
-  })
-      .catch(() => {
-        console.log('Signup system offline');
-      });
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+      .then((data) => {
+        console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+      })
+      .catch(() => console.log('Signup system offline'));
 }
+
+export default handleProfileSignup;

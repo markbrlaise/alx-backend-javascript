426ac32bdbcaa50e65b81541ece52eff83e8852b
mark blaise <markkiguli10@gmail.com>
2023-06-29 10:59:27 +0300

    fixed eslint style

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
-import ClassRoom from './0-classroom';
+import ClassRoom from './0-classroom'
 
-export default function initializeRooms() {
+export default function initializeRooms () {
   const arr = [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34),
-  ];
-  return (arr);
+    new ClassRoom(34)
+  ]
+  return (arr)
 }
